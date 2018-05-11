09f0c2f6d68bc93c3a23adca01d1f8d81378fdf6
unknown <armintoussi@outlook.com>
2018-05-11 15:33:07 -0700

    finished all projects i believe, if needed will come back and do more

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component }             from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'projects',
@@ -9,10 +9,26 @@ import { Component } from '@angular/core';
 
 export class ProjectsComponent {
 
-    isCollapsed: boolean[] = [false, true, true, true];
+    isCollapsed: boolean[] = [false, true, true, true, true, true];
 
-    constructor() { }
+    private fragment: string;
 
+    constructor(private router: Router) { }
+
+    ngOnInit() {
+        //This code allows click and scroll navigation on same page. 
+        this.router.events.subscribe(s => {
+            if(s instanceof NavigationEnd) {
+                const tree = this.router.parseUrl(this.router.url);
+                if(tree.fragment) {
+                    const element = document.querySelector("#" + tree.fragment);
+                    if(element) { 
+                        element.scrollIntoView();
+                    }
+                }
+            }
+        });
+    }
 
     projectCollapse(index: number): void {
         this.isCollapsed[index] = (this.isCollapsed[index])? false: true;
