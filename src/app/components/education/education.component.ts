9f23f49dc3dd6801ede4c4b7953b68d059793a8a
unknown <armintoussi@outlook.com>
2018-05-13 16:58:45 -0700

    added quick link side-nav to education page

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component }             from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'education',
@@ -8,5 +9,22 @@ import { Component } from '@angular/core';
 
 export class EducationComponent { 
 
-    constructor() { }
+    private fragment: string;
+
+    constructor(private router: Router) { }
+
+    ngOnInit() {
+
+        this.router.events.subscribe(s => {
+            if(s instanceof NavigationEnd) {    
+                const tree = this.router.parseUrl(this.router.url);
+                if(tree.fragment) {
+                    const element = document.querySelector("#" + tree.fragment);
+                    if(element) {
+                        element.scrollIntoView();
+                    }
+                }
+            }
+        });
+    }
 }
